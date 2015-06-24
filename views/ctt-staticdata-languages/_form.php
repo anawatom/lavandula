e884ee3eb42c09ae726cdacbfcb3cb1d42e9fc1b
anawatom <anawat.om@gamil.com>
2015-06-25 00:18:29 +0700

    Add create by from login user.
    :)

--- views/ctt-staticdata-languages/_form.php
+++ views/ctt-staticdata-languages/_form.php
@@ -6,6 +6,8 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\CttStaticdataLanguages */
 /* @var $form yii\widgets\ActiveForm */
+
+$currentUser = \Yii::$app->user->getIdentity();
 ?>
 
 <div class="ctt-staticdata-languages-form">
@@ -16,12 +18,12 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'short_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_by')->hiddenInput(['value' => 'DUMMY'])->label(false) ?>
+    <?= $form->field($model, 'created_by')->hiddenInput(['value' => $currentUser->email])->label(false) ?>
 
-    <?= $form->field($model, 'modified_by')->hiddenInput(['value' => 'DUMMY'])->label(false) ?>
+    <?= $form->field($model, 'modified_by')->hiddenInput(['value' => $currentUser->email])->label(false) ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app/frontend', 'Create') : Yii::t('app/frontend', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>

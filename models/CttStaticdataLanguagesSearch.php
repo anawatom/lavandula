657f790bdd693865de8b019144b3beacf96775b9
anawatom <anawat.om@gamil.com>
2015-06-25 00:00:19 +0700

    Add a pagination into ctt_staticdata_language list page.
    :)

--- models/CttStaticdataLanguagesSearch.php
+++ models/CttStaticdataLanguagesSearch.php
@@ -45,6 +45,9 @@ class CttStaticdataLanguagesSearch extends CttStaticdataLanguages
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'pagination' => [
+                'pageSize' => 10,
+            ],
         ]);
 
         $this->load($params);

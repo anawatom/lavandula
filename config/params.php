25fb5e4500c7e817452bff41a34f1b5eb3568f29
anawatom <anawat.om@gamil.com>
2015-07-04 21:08:21 +0700

    Add test global variables.
    :)

--- config/params.php
+++ config/params.php
@@ -2,5 +2,6 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-	'resizeImage' => array('width'=>300, 'height'=>300)
+	'resizeImage' => array('width'=>300, 'height'=>300),
+	'testGlobal' => 'aaaa',
 ];

68fc43089b0e662416102e8dd239b9cdb404fedb
bomcpe11@gmail.com <bomcpe11@gmail.com>
2015-07-12 09:58:08 +0700

    Change DB Pointer for use Mysql 5.6.11

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     // 'dsn' => 'oci:host=103.13.28.239;dbname=103.13.28.239/XE;charset=UTF8',
-    'dsn' => 'mysql:host=103.253.75.56;dbname=unitedla_aci;charset=UTF8',
-    'username' => 'unitedla_aci',
+    'dsn' => 'mysql:host=103.13.28.239;dbname=aci_dev;charset=UTF8',
+    'username' => 'aci_dev',
     'password' => 'aci_2015',
     'charset' => 'utf8',
 ];

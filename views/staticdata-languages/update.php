87f73cd2498e8fbe2d85c554611313481980e609
anawatom <anawat.om@gamil.com>
2015-07-14 22:10:07 +0700

    Edit a word mapping.
    :)

--- views/staticdata-languages/update.php
+++ views/staticdata-languages/update.php
@@ -5,9 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\CttStaticdataLanguages */
 
-$this->title = Yii::t('app/ctt_staticdata_language', 'Update {modelClass}: ', [
-    'modelClass' => 'Ctt Staticdata Languages',
-]) . ' ' . $model->name;
+$this->title = Yii::t('app/ctt_staticdata_language', 'Update Ctt Staticdata Languages') . ': ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app/ctt_staticdata_language', 'Ctt Staticdata Languages'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app/frontend', 'Update');

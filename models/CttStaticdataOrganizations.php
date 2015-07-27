ac70c4b2be4174b76860e4abc7400f59fbb746b0
anawatom <anawat.om@gamil.com>
2015-07-27 23:22:08 +0700

    Add the function to generate timestamp and function to get a ID.
    :)

--- models/CttStaticdataOrganizations.php
+++ models/CttStaticdataOrganizations.php
@@ -3,6 +3,9 @@
 namespace app\models;
 
 use Yii;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "ctt_staticdata_organizations".
@@ -25,8 +28,22 @@ use Yii;
  * @property CttJournals[] $cttJournals
  * @property CttStaticdataAffiliations $affiliation
  */
-class CttStaticdataOrganizations extends \yii\db\ActiveRecord
+class CttStaticdataOrganizations extends ActiveRecord
 {
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_dtm', 'modified_dtm'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'modified_dtm',
+                ],
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
     /**
      * @inheritdoc
      */
@@ -70,6 +87,20 @@ class CttStaticdataOrganizations extends \yii\db\ActiveRecord
             'modified_by' => Yii::t('app', 'Modified By'),
             'modified_dtm' => Yii::t('app', 'Modified Dtm'),
         ];
+    }
+
+    public function getId()
+    {
+        $id = '';
+        $data = parent::find()->where(['name' => $this->name])->one();
+
+        if (empty($data)) {
+            $id = CttSequences::getValue('STATICDATA_ORGANIZATION_SEQ');
+        } else {
+           $id = $data->id;
+        }
+
+        return $id;
     }
 
     /**

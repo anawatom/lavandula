41c2a569c1bf312c5bee5f0cc082bb01fdce5bc2
bomcpe11@gmail.com <bomcpe11@gmail.com>
2015-07-30 20:53:25 +0700

    Add function cRender and setParameter to View Side

--- controllers/base/AppController.php
+++ controllers/base/AppController.php
@@ -8,5 +8,13 @@ class AppController extends \yii\web\Controller
 {
 	public $layout = 'home';
 	public $title = 'ACI';
+	public $parameter=[];
+	
+	protected function setParameter($name, $data){
+		$this->parameter[$name] = $data;
+	}
+	protected function cRender($view){
+		return $this->render($view, $this->parameter);
+	}
 	
 }

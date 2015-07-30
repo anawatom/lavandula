8a8a04d5930658021035e5d0f01747a62347bd5d
bomcpe11@gmail.com <bomcpe11@gmail.com>
2015-07-30 14:42:47 +0700

    Change Database pointer to PROD ENV

--- config/db.php
+++ config/db.php
@@ -2,9 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    // 'dsn' => 'oci:host=103.13.28.239;dbname=103.13.28.239/XE;charset=UTF8',
-    'dsn' => 'mysql:host=103.13.28.239;dbname=aci_dev;charset=UTF8',
-    'username' => 'aci_dev',
+    //'dsn' => 'oci:host=103.13.28.239;dbname=103.13.28.239/XE;charset=UTF8',
+    //'dsn' => 'mysql:host=103.13.28.239;dbname=aci_dev;charset=UTF8',
+	'dsn' => 'mysql:host=122.155.175.5;dbname=chinnawat_aci;charset=UTF8',
+    'username' => 'chinnawat_aci',
     'password' => 'aci_2015',
     'charset' => 'utf8',
 ];

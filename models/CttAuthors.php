1f8ff26c2bbfe1a509250a55c31f4ec9540f78ae
anawatom <anawat.om@gamil.com>
2015-08-17 21:46:41 +0700

    Adding a function to generate the timestamp fields.
    :)

--- models/CttAuthors.php
+++ models/CttAuthors.php
@@ -3,6 +3,9 @@
 namespace app\models;
 
 use Yii;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "ctt_authors".
@@ -38,8 +41,22 @@ use Yii;
  * @property CttStaticdataOrganizations $organization0
  * @property CttReferenceAuthors[] $cttReferenceAuthors
  */
-class CttAuthors extends \yii\db\ActiveRecord
+class CttAuthors extends ActiveRecord
 {
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_dtm', 'modified_dtm'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'modified_dtm',
+                ],
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
     /**
      * @inheritdoc
      */
